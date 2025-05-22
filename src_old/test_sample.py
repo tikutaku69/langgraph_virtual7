400ad280f6a6b3a8527bb8a606d891e7fdb02973
taku <moldigai@yahoo.co.jp>
2025-05-22 23:18:18 +0900

    2つのジョブが並列で実行できるか

--- src_old/test_sample.py
+++ src_old/test_sample.py
@@ -6,7 +6,7 @@ def test_add_numbers_1():
 
 def test_add_numbers_2():
     print("正の整数と負の整数の足し算")
-    assert add_number(-1, 1) == 1
+    assert add_number(-1, 1) == 0
 
 def test_add_numbers_3():
     print("ゼロ同士の足し算")
@@ -14,7 +14,7 @@ def test_add_numbers_3():
 
 def test_add_numbers_4():
     print("負の整数同士の足し算")
-    assert add_number(-1, -1) == -3
+    assert add_number(-1, -1) == -2
 
 def test_add_numbers_5():
     print("大きな正の整数同士の足し算")
