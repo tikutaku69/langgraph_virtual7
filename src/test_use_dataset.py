cda2e7557f61c0f447300d4a215dec3a15467787
taku <moldigai@yahoo.co.jp>
2025-05-22 23:15:00 +0900

    閾値をtestのほうで決めてるのが微妙だが、平均点出してチェックするtest作れた

--- src/test_use_dataset.py
+++ src/test_use_dataset.py
@@ -2,7 +2,11 @@ from use_dataset import evaluate_dataset
 
 def test_add_numbers_1():
     scores = evaluate_dataset()
-    print(scores)
+
+    assert len(scores) > 0, "スコアが空のリストです"
+    border_score = 95
+    average_score = round(sum(scores) / len(scores), 1)
+    assert average_score >= border_score, f"平均点が{border_score}点未満です。現在の平均点: {average_score:.2f}"
 
 if __name__ == "__main__":
     test_add_numbers_1()
